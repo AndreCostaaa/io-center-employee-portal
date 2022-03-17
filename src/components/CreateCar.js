ef8e740cf1616814ae0f1a49be109e353dc8a7fb
André Costa <55462127+AndreCostaaa@users.noreply.github.com>
2022-03-17 23:34:15 +0100

    testing axios

--- src/components/CreateCar.js
+++ src/components/CreateCar.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useRef } from "react";
 import { Button, Card, Col, Form, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
@@ -5,7 +6,37 @@ import { useNavigate } from "react-router-dom";
 export default function CreateClient() {
   const prenomRef = useRef();
   const nomRef = useRef();
+  const registrationRef = useRef();
   const navigate = useNavigate();
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    //let file = registrationRef.files[0]
+    let file = registrationRef.current.files[0];
+    console.log(file);
+    //console.log(file.type.match("/image.*/"));
+
+    var fd = new FormData();
+    fd.append("id", 2);
+    fd.append("car_registration_photo", file);
+    fd.append("brand", "bmw");
+    fd.append("chassis_no", "12");
+    fd.append("license_plate", "fr123456");
+    console.log(fd);
+    await axios({
+      method: "post",
+      url: "/vehicle",
+      data: fd,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then(function (res) {
+        console.log(res);
+        //console.log(res.data);
+      })
+      .catch(function (error) {
+        console.log(error.toJSON());
+      });
+  }
   return (
     <>
       <Card>
@@ -40,18 +71,20 @@ export default function CreateClient() {
             </Form.Group>
             <Form.Group>
               <Form.Label>Carte Grise</Form.Label>
-              <Form.Control type="file" />
+              <Form.Control ref={registrationRef} type="file" />
             </Form.Group>
             <Form.Group>
               <Form.Label>Photos</Form.Label>
               <Form.Control type="file" multiple />
             </Form.Group>
             <Button
-              onClick={() => {
-                navigate("/create-service");
-              }}
+              onClick={
+                //navigate("/create-service");
+                //axios.post;
+                handleSubmit
+              }
               className="w-100 mt-3"
-              type="submit"
+              //type="submit"
             >
               Create
             </Button>
